fb200d35690c1727959222724f53348d79f17b42
kaidong pei <kaidongpei@kaidongs-MacBook-Pro-2.local>
2017-12-27 19:15:37 -0600

    newBranch

--- tryGit/ViewController.swift
+++ tryGit/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 //add line
     //new
+    //new again
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
